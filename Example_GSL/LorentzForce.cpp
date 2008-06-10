e42eecf71d6da5303428a8677435b5480ba09741
chuck <chuck@f72a1731-e78b-9d40-b9e2-f1fb8c3fba11>
2008-06-10 15:11:21 +0000

    Ajouté la force de Lorentz 2D pour le faisceau polirisé radialement.
    git-svn-id: file:///home/charles/Documents/SciWork/SVN/SVN_LaserAccel/trunk@16 f72a1731-e78b-9d40-b9e2-f1fb8c3fba11

--- Example_GSL/LorentzForce.cpp
+++ Example_GSL/LorentzForce.cpp
@@ -10,25 +10,34 @@
 #include"LorentzForce.hpp"
 
 /****************** External functions implementation *************************/
-int RPLB_3D_LorentzForce(const double t, const double y[], 
+int RPLB_3D_LorentzForce(const double t, double y[], 
                           double dydt[], void* p)
 {
+    /************** Extract parameters from the external structure ************/ 
     RPLB_SimParams *pp = (RPLB_SimParams*)p;
-    const double q = pp->q;
-    const double m = pp->m;
+    const double q_m = pp->q/pp->m;
+    const double Er = pp->emf.Er;
+    const double Ez = pp->emf.Ez;
+    const double B_theta = pp->emf.B_theta;
     
+    /************** Initialize some local variables ***************************/
     const double r = y[0];
     const double z = y[1];
     const double vr = y[2];
     const double vz = y[3];
     const double inv_gamma = sqrt(1 - (vr*vr)*inv_co_square
                                      - (vz*vz)*inv_co_square);
+    const double vdotE = vr*Er + vz*Ez;
     
+    /************** Equations of motion ***************************************/
     dydt[0] = vr;
     dydt[1] = vz;
-    dydt[2] = 0.0;
-    dydt[3] = 0.0;
+    dydt[2] = q_m*inv_gamma*(Er - vz*B_theta - vr*vdotE*inv_co_square);
+    dydt[3] = q_m*inv_gamma*(Ez + vr*B_theta - vz*vdotE*inv_co_square);
     
+    /************** Returns a positive value for r ****************************/
+    if(r < 0.0) y[0] = -y[0];
+
     return GSL_SUCCESS;
 }
 
